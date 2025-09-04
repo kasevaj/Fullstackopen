b603f625d92575bccb2c11b1a8e5a17f6444c24f
Jasmin Kaseva <jasmin.kaseva@gmail.com>
2025-09-04 20:13:28 +0300

    1.7

--- Osa1/src/App.jsx
+++ Osa1/src/App.jsx
@@ -1,30 +1,47 @@
 import { useState } from 'react'
 
-const Button = (props) => (
-
-  <button onClick={props.onClick}>
-    {props.text}
-  </button>
-
-)
-
 const App = () => {
-  // save clicks of each button to its own state
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGoodClick = () => {
-    setGood(good + 1)
-    console.log("jotain tapahtui good")
+    const newGood = good + 1
+    const newTotal = newGood + bad + neutral
+    const newAverage = (newGood - bad) / newTotal
+    const newPositive = (newGood / newTotal * 100)
+    setGood(newGood)
+    setTotal(newTotal)
+    setAverage(newAverage)
+    setPositive(newPositive)
+    console.log("total", total)
   }
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-    console.log("jotain tapahtui neutral")
+    const newNeutral = neutral + 1
+    const newTotal = good + bad + newNeutral
+    const newAverage = (good - bad) / newTotal
+    const newPositive = (good / newTotal * 100)
+    setNeutral(newNeutral)
+    setTotal(newTotal)
+    setAverage(newAverage)
+    setPositive(newPositive)
+    console.log("neutral", neutral)
+    console.log("total", total)
   }
   const handleBadClick = () => {
-    setBad(bad + 1)
-    console.log("jotain tapahtui bad")
+    const newBad = bad + 1
+    const newTotal = newBad + good + neutral
+    const newAverage = (good - newBad) / newTotal
+    const newPositive = (good / newTotal * 100)
+    setBad(newBad)
+    setTotal(newTotal)
+    setAverage(newAverage)
+    setPositive(newPositive)
+    console.log("total", total)
   }
 
   return (
@@ -33,19 +50,25 @@ const App = () => {
       <header>
         Give Feedback
       </header>
-      <Button onClick={handleGoodClick} text="good"></Button>
-      <Button onClick={handleNeutralClick} text="neutral"></Button>
-      <Button onClick={handleBadClick} text="bad"></Button>
+      <button onClick={handleGoodClick}> good</button>
+      <button onClick={handleNeutralClick}> neutral</button>
+      <button onClick={handleBadClick}> bad</button>
       <p>
         Statistics
       </p>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>total {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </div>
   )
 }
 
 export default App
 
-// kolme nappulaa, ja niiden omat summat
+// lisätään kokonaissumma, keskiarvo ja positiivisten arvosteluiden prosenttimäärä
+
+// keskiarvo kun good = 1, neutral = 0 ja bad = -1
+// average = (good - bad) / total ??
