3f97cc2ab68170a0729c807d3bb9f5bf5bb68f3c
Jasmin Kaseva <jasmin.kaseva@gmail.com>
2025-09-15 15:23:02 +0300

    2.2

--- Osa2/courseinfo2/src/App.jsx
+++ Osa2/courseinfo2/src/App.jsx
@@ -15,7 +15,14 @@ const Part = (props) => (
   </p>
 )
 
-const Total = (props) => <p>Number of exercises {props.total}</p>
+const Total = (props) => {
+  const total = props.course.parts.reduce((sum, part) => sum + part.exercises, 0)
+  console.log("total", total)
+  return (
+    <p>Number of exercises: {total}</p>
+  )
+}
+
 
 const Course = (props) => {
   console.log(props, 'props in use')
@@ -30,6 +37,8 @@ const Course = (props) => {
           // huom part on nyt se josta luodaan uusi array
         )}
       </ul>
+      <Total course={props.course} />
+
     </div>
   )
 }
@@ -54,6 +63,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
